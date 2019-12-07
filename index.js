f65c38302b0f7a00d6ba3d98f52063d839bcae32
Alexis Kondilis <kondilisalexis@gmail.com>
2019-12-07 05:09:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const names = [ 'Lisa', 'Kaitlin', 'Jan' ]
 
 function writeCards(names, 'surprise'){
 
-for ( let i = 0; i < name.length; i++){
+for (let i=0; i < name.length; i++){
   names.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
  }
  return names;
